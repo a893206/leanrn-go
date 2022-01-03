89b05bd1e223f292f314088163501d823654fec2
CR <931009686@qq.com>
2022-01-04 01:28:38 +0800

    feat: 7-5 服务器统一出错处理2

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -12,12 +12,25 @@ type appHandler func(writer http.ResponseWriter,
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
 			log.Printf("Error occurred "+
 				"handling request: %s",
 				err.Error())
 			code := http.StatusOK
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
+
 			switch {
 			case os.IsNotExist(err):
 				code = http.StatusNotFound
@@ -31,8 +44,13 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
