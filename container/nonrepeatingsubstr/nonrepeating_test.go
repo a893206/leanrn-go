7630c883edf3952457c7dc2586dc28a7d3ed8dfd
CR <931009686@qq.com>
2022-01-07 01:54:21 +0800

    feat: 8-3 使用pprof进行性能调优

--- container/nonrepeatingsubstr/nonrepeating_test.go
+++ container/nonrepeatingsubstr/nonrepeating_test.go
@@ -33,7 +33,12 @@ func TestSubstr(t *testing.T) {
 
 func BenchmarkSubstr(b *testing.B) {
 	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s += s
+	}
+	b.Logf("len(s) = %d", len(s))
 	ans := 8
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		actual := lengthOfNonRepeatingSubStr(s)
