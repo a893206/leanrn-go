247f5242487f6466cdc64f143b70e6a2412d8d23
CR <931009686@qq.com>
2022-01-10 01:15:49 +0800

    feat: 9-2 go语言的调度器

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
-	"runtime"
 	"time"
 )
 
 func main() {
 	var a [10]int
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 1000; i++ {
 		go func(i int) {
 			for {
-				a[i]++
-				runtime.Gosched()
+				fmt.Printf("Hello from goroutine %d\n", i)
 			}
 		}(i)
 	}
-	time.Sleep(time.Millisecond)
+	time.Sleep(time.Minute)
 	fmt.Println(a)
 }
