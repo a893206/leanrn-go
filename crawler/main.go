72aa859babb74ef695f1902f5da222f483e46471
CR <931009686@qq.com>
2022-01-19 00:34:01 +0800

    feat: 15-2 简单调度器

--- crawler/main.go
+++ crawler/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"learngo/crawler/engine"
+	"learngo/crawler/scheduler"
 	"learngo/crawler/zhenai/parser"
 )
 
 func main() {
-	engine.Run(engine.Request{
+	e := engine.ConcurrentEngine{
+		Scheduler:   &scheduler.SimpleScheduler{},
+		WorkerCount: 10,
+	}
+
+	e.Run(engine.Request{
 		Url:        "http://www.zhenai.com/zhenghun",
 		ParserFunc: parser.ParseCityList,
 	})
