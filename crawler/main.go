362429a44e5b8b66dcc06284eb24c94ae124d239
CR <931009686@qq.com>
2022-02-09 23:34:00 +0800

    feat: 15-6 更多城市

--- crawler/main.go
+++ crawler/main.go
@@ -12,8 +12,13 @@ func main() {
 		WorkerCount: 100,
 	}
 
+	//e.Run(engine.Request{
+	//	Url:        "http://www.zhenai.com/zhenghun",
+	//	ParserFunc: parser.ParseCityList,
+	//})
+
 	e.Run(engine.Request{
-		Url:        "http://www.zhenai.com/zhenghun",
-		ParserFunc: parser.ParseCityList,
+		Url:        "http://www.zhenai.com/zhenghun/shanghai",
+		ParserFunc: parser.ParseCity,
 	})
 }
